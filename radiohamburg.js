af79d0df2db873889bdc503cfcecdbf5f8b78481
xyNNN <philipp.braeutigam@googlemail.com>
2015-04-09 21:23:57 +0200

    Added music title to CLI window

--- radiohamburg.js
+++ radiohamburg.js
@@ -11,6 +11,11 @@ var url = 'http://radiohamburg.hoerradar.de/radiohamburg-live-mp3-192';
 icecast.get(url, function (response) {
     console.log('Playing Radio Hamburg...');
 
+    response.on('metadata', function (metadata) {
+        var parsed = icecast.parse(metadata);
+        console.log(parsed.StreamTitle);
+    });
+
     response.pipe(new lame.Decoder())
         .pipe(new Speaker());
 });
